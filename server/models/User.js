e64b1ac4aad5f0d94566ea7ca55006d53410a81f
ZaZenZe <desayan2014@gmail.com>
2025-06-28 17:17:51 +0200

    fix : login auth

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, trim: true, unique: true }, // Added username field
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, lowercase: true },
   password: { type: String, required: true, minlength: 6 },
